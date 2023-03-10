e51c059bfcf0c9db7a3899718c73c1f7ebbbf74a
MrBiiig <457965950@qq.com>
2023-03-10 18:37:01 +0800

    布局css写了一部分

--- src/pages/cssLayouts/index.tsx
+++ src/pages/cssLayouts/index.tsx
@@ -7,14 +7,14 @@ const CssLayouts = () => {
   return (
     <React.Fragment>
       <div className="page_cssLayouts_box">
-        <div className="container holy">
-          <div className="holy_header bg_color_4">header</div>
-          <div className="container">
-            <div className="holy_grail_center_content bg_color_1">center</div>
-            <div className="holy_grail_left_content bg_color_2">left</div>
-            <div className="holy_grail_right_content bg_color_3">right</div>
+        <div className="bfc_container holy_grail_layout">
+          <div className="header bg_color_4">header</div>
+          <div className="bfc_container">
+            <div className="center bg_color_1">center</div>
+            <div className="left bg_color_2">left</div>
+            <div className="right bg_color_3">right</div>
           </div>
-          <div className="holy_footer bg_color_5">footer</div>
+          <div className="footer bg_color_5">footer</div>
         </div>
       </div>
     </React.Fragment>
